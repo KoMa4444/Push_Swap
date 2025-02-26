643ea7e2049c5ed03a04ac682a1783c95a5a2fe3
KoMa4444 <marcelokollar@gmail.com>
2025-02-26 12:27:27 +0100

    pa q este subido

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -5,7 +5,23 @@
 
 typedef struct stack_s {
 	int			val;
-	int			
-}	stack_t;
+	struct stack_s prev;
+	struct stack_s next;
+}			stack_t;
+
+// main.c
+
+
+// utils
+
+
+// operations
+void	push(stack_t *src, stack_t *dst);
+void	swap(stack_t *stack);
+void	rotate(stack_t *stack);
+void	r_rotate(stack_t *stack);
+
+// error handle
+void	free_stack(stack_t **stack);
 
 #endif
